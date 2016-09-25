722d124fea0b1a2f6bc9b739954a8ed221791c7f
B0dz1o <B0dz1o@users.noreply.github.com>
2016-09-25 23:09:08 +0200

    Created project

---
+++ GoT_wiki/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GoT_wiki
+//
+//  Created by Piotr Bogdan on 9/25/16.
+//  Copyright © 2016 Piotr Bogdan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GoT_wiki/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  GoT_wiki
+//
+//  Created by Piotr Bogdan on 9/25/16.
+//  Copyright © 2016 Piotr Bogdan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
