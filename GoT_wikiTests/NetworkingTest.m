ccf36ca84ae6b6b523858f4a0001f56e373b2d80
Piotr Bogdan <bogdzio@o2.pl>
2016-09-28 19:34:08 +0200

    Complete network classes testing

--- GoT_wikiTests/NetworkingTest.m
+++ GoT_wikiTests/NetworkingTest.m
@@ -8,10 +8,12 @@
 
 #import <XCTest/XCTest.h>
 #import "RequestFactory.h"
+#import "HTTPBuilder.h"
 
 @interface NetworkingTest : XCTestCase
 
 @property RequestFactory* factory;
+@property HTTPBuilder* builder;
 
 @end
 
@@ -22,6 +24,7 @@
 - (void)setUp {
     [super setUp];
     [self setFactory:[RequestFactory sharedObject]];
+    [self setBuilder:[[HTTPBuilder alloc] init]];
 }
 
 - (void)tearDown {
@@ -35,12 +38,52 @@
 }
 
 - (void)testFactoryCreation {
-    XCTAssertNil([factory createTask:nil withHandler:nil]);
-    XCTAssertNotNil([factory createTask:CHARACTER_LIST withHandler:nil]);
+    XCTAssertNil([factory runTask:nil withHandler:nil]);
+    XCTAssertNoThrow([factory runTask:CHARACTER_LIST withHandler:nil]);
+    XCTAssertNotNil([factory runTask:CHARACTER_LIST withHandler:nil]);
+    XCTAssertNoThrow([factory runTask:CHARACTER_LIST withHandler:
+                      ^(NSData *d, NSURLResponse *r, NSError *e){}]);
+    XCTAssertNotNil([factory runTask:CHARACTER_LIST withHandler:
+                     ^(NSData *d, NSURLResponse *r, NSError *e){}]);
 }
 
-- (void)testPerformanceExample {
+- (void) testBuilderParamsFormatter {
+    [[self builder] useParameters:@{@"expand":@1, @"category":@"Characters", @"limit":@75}];
+    XCTAssertTrue([[[self builder] paramsString] isEqualToString:@"?expand=1&category=Characters&limit=75"]);
+    [[self builder] useParameters:nil];
+    XCTAssertNil([[self builder] paramsString]);
+    [[self builder] useParameters:@{@"limit":@75}];
+    XCTAssertTrue([[[self builder] paramsString] isEqualToString:@"?limit=75"]);
+}
+
+- (void) testBuilder {
+    [[[[self builder] useDefaultBaseURL] usePath:@"Articles/Top"]
+     useParameters: @{@"expand":@1,
+                      @"category":@"Characters",
+                      @"limit":@75}];
+    NSString * addr = [[[[[self builder] buildTask] currentRequest] URL] absoluteString];
+    XCTAssertTrue([addr isEqualToString:@"http://gameofthrones.wikia.com/api/v1/Articles/Top?expand=1&category=Characters&limit=75"]);
+}
+
+- (void)testBuilderPerformance {
     [self measureBlock:^{
+        for (int i = 0; i < 100; ++i) {
+            [[[[[self builder] useDefaultBaseURL]
+               usePath:@"Articles/Top"]
+              useParameters: @{@"expand":@1,
+                              @"category":@"Characters",
+                              @"limit":@75}]
+             buildTask];
+        }
+    }];
+}
+
+- (void) testFactoryPerformance {
+    [self measureBlock:^{
+        for(int i = 0; i < 100; ++i) {
+            [[self factory] runTask:CHARACTER_LIST
+                        withHandler:^(NSData *d, NSURLResponse *r, NSError *e) {}];
+        }
     }];
 }
 
