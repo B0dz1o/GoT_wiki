b281d6a60a007aa9e38de5ee0748b85d43adb21f
Piotr Bogdan <bogdzio@o2.pl>
2016-09-30 02:37:51 +0200

    Timers for Travis output. Reverted gem xcodeproj to 1.3.0

--- allScripts.sh
+++ allScripts.sh
@@ -3,14 +3,14 @@
 travis_time_start() {
   travis_timer_id=$(printf %08x $(( RANDOM * RANDOM )))
   travis_start_time=$(travis_nanoseconds)
-  echo "travis_time:start:$travis_timer_id"
+  echo -en "travis_time:start:$travis_timer_id\r${ANSI_CLEAR}"
 }
 
 travis_time_finish() {
   local result=$?
   travis_end_time=$(travis_nanoseconds)
   local duration=$(($travis_end_time-$travis_start_time))
-  echo  "\ntravis_time:end:$travis_timer_id:start=$travis_start_time,finish=$travis_end_time,duration=$duration"
+  echo -en "\ntravis_time:end:$travis_timer_id:start=$travis_start_time,finish=$travis_end_time,duration=$duration\r${ANSI_CLEAR}"
   return $result
 }
 
@@ -39,14 +39,18 @@ then
 fi
 
 #UI tests for iPhone 4S
+travis_time_start
 bash scripts/ip4s.sh
+travis_time_finish
 if [ $? -ne 0 ];
 then
 	exit 1
 fi
 
 #UI tests for iPhone 5S
+travis_time_start
 bash scripts/ip5s.sh
+travis_time_finish
 if [ $? -ne 0 ];
 then
 	exit 1
